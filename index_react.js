7856c0188a02e79cb8328db64c7c70943e4d956e
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2022-11-01 18:56:13 +0530

    Added the index_react.js file which helps in interacting with the react application

---
+++ index_react.js
@@ -0,0 +1,70 @@
+const path = require('path');
+
+const { app, BrowserWindow, ipcMain } = require('electron');
+const isDev = require('electron-is-dev');
+
+const { processExcelFile } = require('./spreadsheet/excel');
+const { processRowTally } = require("./spreadsheet/excel_tally");
+const { tallyCheckServer } = require("./tally/request");
+
+let mainWindow;
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      // enableRemoteModule: true,
+      contextIsolation: false
+    },
+  });
+
+  // and load the index.html of the app.
+  // win.loadFile("index.html");
+  mainWindow.loadURL(
+      isDev
+          ? 'http://localhost:3000'
+          : `file://${path.join(__dirname, '../build/index.html')}`
+  );
+
+  // Open the DevTools.
+  if (isDev) {
+    // win.webContents.openDevTools({ mode: 'detach' });
+  }
+
+  tallyCheckServer()
+      .then(response => console.log(`response: ${JSON.stringify(response)}`))
+      .catch(error => console.error(`error: ${JSON.stringify(error)}`));
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+ipcMain.on('excel:submit', (event, files) => {
+  files.forEach(filePath => {
+    console.log(`Received File:`, filePath);
+    processExcelFile(filePath, processRowTally);
+  });
+
+  mainWindow.webContents.send('excel:processed', files);
+})
+
